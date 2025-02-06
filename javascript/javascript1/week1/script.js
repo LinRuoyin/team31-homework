34f3e077cf2d4bb47eca8bd31ef0e2d8bdc9f21a
LinRuoyin <nina.linn@outlook.com>
2025-02-06 19:13:36 +0100

    Revised version

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -6,10 +6,10 @@ const age = yearFuture - yearOfBirth
 console.log(`You will be ${age} years old in ${yearFuture}`)
 
 // Homework 2 : Goodboy-Oldboy (A dog age calculator) 
-let dogYearOfBirth = 2017
-let dogYearFuture = 2027
-let dogYear = dogYearFuture-dogYearOfBirth
-let shouldShowResultInDogYears = true
+const dogYearOfBirth = 2017
+const dogYearFuture = 2027
+const dogYear = dogYearFuture-dogYearOfBirth
+const shouldShowResultInDogYears = true
 if(shouldShowResultInDogYears){
   console.log(`Your dog will be ${dogYear*7} dog years old in ${dogYearFuture}`)
 }else {
@@ -19,23 +19,23 @@ if(shouldShowResultInDogYears){
 // Homework 3 : Housey pricey (A house price estimator)
 
 
-function housePrice(width,depth,height,gardenSizeM2){
+function estimatedHousePrice(width,depth,height,gardenSizeM2){
   let volumeInMeters= width*depth*height
-  let housePrice = volumeInMeters*2.5*1000 + gardenSizeM2*300
-  return housePrice
+  let estimatedHousePrice = volumeInMeters*2.5*1000 + gardenSizeM2*300
+  return estimatedHousePrice
 }
 
-if(housePrice(8,10,10,100)<2500000){
+if(estimatedHousePrice(8,10,10,100)<2500000){
   console.log('Peter paid too much')}
   else{
     console.log('Peter paid too little')
   } 
 
-  if (housePrice(5,11,8,70)<1000000){
-    console.log('Julia paid too much')
-  }else{
-    console.log('Julia paid too little')
-  }
+if (estimatedHousePrice(5,11,8,70)<1000000){
+  console.log('Julia paid too much')
+}else{
+  console.log('Julia paid too little')
+}
 
 // Homework 4 : Ez Namey (Startup name generator) Optional 
 const firstWords = ["Easy", "Awesome", "Creative", "Dynamic", "Bold", "Bright", "Next", "Smart", "Happy", "Future"]
