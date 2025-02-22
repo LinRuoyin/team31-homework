09f8ddc2c6b9f6b42a973f5c87546a6f1c96863b
LinRuoyin <nina.linn@outlook.com>
2025-02-22 10:43:11 +0100

    Revised according to mentor's comment

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -9,9 +9,12 @@
 // console.log(fullname1);
 // console.log(fullname2);
 
-//formal fullname
+//formal fullname (revised according to comment)
 function getFullname(firstname, surname, useFormalName) {
-  if (useFormalName) {
+  if (!firstname || !surname){
+    return "Error: Please make sure both names are provided"
+    // In this way, if the first name and surname is not provided, it will return Error 
+  }else if(useFormalName) {
     return "Lord" + firstname + " " + surname;
   } else {
     return firstname + " " + surname;
@@ -21,19 +24,25 @@ function getFullname(firstname, surname, useFormalName) {
 // But what if the person is a woman? Describe how you would fix the getFullname so it also works for women?
 
 // //  function getFullname(firstname, surname, gender, useFormalName) {
+/* There's a repetition of firstname + " " + surname;
+*/
+// const fullName= firstname + " " + surname
 // if(useFormalName){
 //   if(gender='female'){
-//     return "Lady" + firstname + " " + surname;}
-//   else{ return "Lord" + firstname + " " + surname;}
+//     return "Lady" + fullName;}
+//   else{ return "Lord" + fullName;}
 // } else {
-//   return firstname + " " + surname;}
+//   return firstname + fullName;}
 // }
 
 // Event application 
 function getEventWeekday(dayToEvent){
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const today = new Date()
-  const todayIndex = today.getDay()
+  // const today = new Date()
+  // const todayIndex = today.getDay()
+// revise below
+const todayIndex= new Date().getDay()
+
   const dayOfEventIndex=  (dayToEvent + todayIndex) % 7;
   return days[dayOfEventIndex];
 }
@@ -60,41 +69,54 @@ console.log(whatToWear(50));
 // Student manager 
 const class07Students = [];
 function addStudentToClass(studentName) {
-  if (studentName === "Queen" && !class07Students.includes('Queen')) {
-   class07Students.push(studentName);
-   return class07Students
-  } else if (class07Students.includes(studentName)){
-     console.log(`Student ${studentName} is already in the class`);
-     return 'error'
-  } else if (studentName === "") {
+  //Revise: change the order of conditions. 
+  // First check if student's name is empty 
+  // second, check for duplicates
+  // third check for queen
+  // fourth check for class capacity
+  // fifth, add the student 
+  if (studentName === "") {
   console.log("Please enter a valid name");
    return 'error'
-  } else if (class07Students.length >= 6) {
+  } 
+  
+  if(class07Students.includes(studentName)){
+     console.log(`Student ${studentName} is already in the class`);
+     return 'error'
+  } 
+  
+  if(studentName === "Queen" && !class07Students.includes("Queen")) {
+    class07Students.push(studentName);
+    return class07Students;
+  } 
+
+  if (class07Students.length >= 6) {
     console.log("Cannot add more students to class 07");
     return "error";
-  } else {
+  }
+
+  //Pass all the above, so push the student to the array
     class07Students.push(studentName);
     return class07Students;
   }
 
 
-}
 
 function getNumberOfStudents() {
   return class07Students.length;
 }
 
-console.log(addStudentToClass('Benjamin'))
-console.log(addStudentToClass('Nina'))  
-console.log(addStudentToClass('Nina'))
-console.log(addStudentToClass('Nancy'))
-console.log(addStudentToClass('Tom'))
-console.log(addStudentToClass('Jerry'))
-console.log(addStudentToClass(''))
-console.log(addStudentToClass('Gary'))
-console.log(addStudentToClass('Queen'))
-console.log(addStudentToClass('Queen'))
-console.log(addStudentToClass('Jenny'))
+addStudentToClass('Benjamin')
+addStudentToClass('Nina')
+addStudentToClass('Nancy')
+addStudentToClass('Tom')
+addStudentToClass('Nina')
+addStudentToClass('Jerry')
+addStudentToClass('')
+addStudentToClass('Gary')
+addStudentToClass('Queen')
+addStudentToClass('Queen')
+addStudentToClass('Jenny')
 
 console.log(getNumberOfStudents())
 
@@ -102,15 +124,27 @@ console.log(getNumberOfStudents())
 // Candy helper optional 
 const boughtCandyPrices = [];
 function addCandy(candyType, weight){
-  if(candyType === "sweet"){
+
+  if(typeof candyType !== "string") {
+  console.log("Error: Candy type must be a string")
+  return
+}
+
+const lowerCaseCandyType= candyType.toLowerCase()
+
+  if(lowerCaseCandyType === "sweet"){
     boughtCandyPrices.push(weight * 0.5);
-  } else if(candyType === "chocolate"){
+  } else if (lowerCaseCandyType === "chocolate") {
     boughtCandyPrices.push(weight * 0.7);
-  } else if(candyType === "toffee"){
+  } else if (lowerCaseCandyType === "toffee") {
     boughtCandyPrices.push(weight * 1.1);
-  } else if(candyType === "chewing-gum"){
+  } else if (lowerCaseCandyType === "chewing-gum") {
     boughtCandyPrices.push(weight * 0.03);
+  } else {
+    console.log("Error: Please enter the right candy type")
+    return
   }
+
   return boughtCandyPrices
 }
 
